d222ebf81a565a6b12ddeabdbd505d7655ab0514
Jiang Sheng <gisonrg@gmail.com>
2016-10-13 16:39:45 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -53,12 +53,6 @@ gulp.task('minify-js', function() {
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
-
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
-
     gulp.src(['node_modules/magnific-popup/dist/*'])
         .pipe(gulp.dest('vendor/magnific-popup'))
 
